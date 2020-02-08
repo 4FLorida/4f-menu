139c54563bedf4576d7dfc0dfeac4f73024cc685
Berkay Burak <berkay.burak.misirlioglu@gmail.com>
2020-02-08 15:30:50 +0300

    css changes

--- components/Product/ProductCardImage.js
+++ components/Product/ProductCardImage.js
@@ -1,13 +1,12 @@
 import styled from 'styled-components';
 
 const ProductCardImage = styled.img`
-  height:60px;
-  width:15%;
-  margin:5px 1rem 5px 10px;
+  height: 60px;
+  width: 15%;
+  margin: 5px 1rem 5px 10px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-border-radius: 5px;
-  ${({src}) => `background-image:${src};`}
-  
+  border-radius: 5px;
+  ${({ src }) => `background-image:${src};`}
 `;
 
 export default ProductCardImage;
--- components/Product/ProductName.js
+++ components/Product/ProductName.js
@@ -3,13 +3,14 @@ import styled from 'styled-components';
 const ProductName = styled.p`
   color: #323c47;
   font-size: 15px;
-  width: 40%;
+  width:50%;
 
   font-family: 'Poppins', sans-serif;
   font-style: Regular;
   font-weight: normal;
   font-size: 15px;
   line-height: 40px;
+
 `;
 
 export default ProductName;

64445fccf324e48f4d1ae39bc129a627c428a962
Berkay Burak <berkay.burak.misirlioglu@gmail.com>
2020-02-08 15:28:08 +0300

    Update Searchbox.js
    Searchbox component css changes

--- components/Searchbox.js
+++ components/Searchbox.js
@@ -2,24 +2,34 @@ import styled from 'styled-components';
 
 const Searchbox = styled.input`
   height: 32px;
-  width: 260px;
+  width: 83%;
   border-radius: 20px;
-  margin-right:26px;
-  margin-top:1px;
+  margin-top: 1px;
   background: #ffffff;
   border: 1px solid #e0e0e0;
-
-  :focus {
-    outline: none;
-    box-shadow: 0 0 0.1rem 0.5pt #007bff;
-  }
-
+  margin-right: 10px;
   background-color: #ffffff;
   background-size: 16px;
   background-image: url('https://svgshare.com/i/Hfw.svg');
   background-position: 10px 10px;
   background-repeat: no-repeat;
   padding-left: 40px;
+
+  :focus {
+    outline: none;
+    --webkit-box-shadow: 0 8px 8px -6px grey;
+    -moz-box-shadow: 0 8px 8px -6px grey;
+    box-shadow: 0 8px 8px -6px grey;
+  }
+  @media (min-width: 576px) and (max-width: 767.98px) {
+    width: 50%;
+  }
+  @media (min-width: 768px) and (max-width: 991.98px) {
+    width: 75%;
+  }
+  @media (min-width: 992px) and (max-width: 1199.98px) {
+    width: 80%;
+  }
 `;
 
 export default Searchbox;

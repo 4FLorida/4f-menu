2cfffc0fb1cc28c2bf41d1de05f500ec93d3acf3
Berkay Burak <berkay.burak.misirlioglu@gmail.com>
2020-02-08 15:27:01 +0300

    Product card media queries

--- components/Product/ProductCard.js
+++ components/Product/ProductCard.js
@@ -2,13 +2,27 @@ import styled from 'styled-components';
 
 const ProductCard = styled.div`
   height: 70px;
-  width:49.5%;
+  width:49.8%;
   margin-bottom:1px;
   margin-right:1px;
   display:flex;
   background-color: white;
   border:0.5px solid lightgrey;
-  border-
+
+
+  @media (min-width: 480px) {
+  }
+  @media (min-width: 576px) and (max-width: 767.98px) {
+    width: 100%;
+  }
+  @media (min-width: 768px) and (max-width: 991.98px) {
+    width:100%;
+  }
+  @media (min-width: 992px) and (max-width: 1199.98px) {
+    width:49.6%;
+  }
+  @media (min-width: 1200px) {
+  }
 `;
 
 export default ProductCard;

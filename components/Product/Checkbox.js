310e9c2331149185ecf673964bc4295476346f42
Berkay Burak <berkay.burak.misirlioglu@gmail.com>
2020-02-08 15:27:14 +0300

    Create checkbox component

---
+++ components/Product/Checkbox.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Checkbox = styled.input.attrs({type:'checkbox'}) `
+
+  width:25px;
+  height:25px;
+  border: 2px solid #FFFFFF;
+  border-radius: 4px;
+  
+`;
+
+export default Checkbox;
